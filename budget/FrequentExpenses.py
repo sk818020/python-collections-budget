5ba8a925b11176a2454a99d1221014786fcb3164
sk818020 <jared.m.heiner@gmail.com>
2021-04-07 23:20:59 +0200

    Second Push

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,12 +1,17 @@
-from . import Expense
+from budget import Expense
+import collections
 
 expenses = Expense.Expenses()
 
-expenses.read_expenses('data/spending_data.csv')
+expenses.read_expenses(r'C:\Users\jared\PycharmProjects\python-collections-budget\data\spending_data.csv')
 
 spending_categories = []
 
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
+spending_counter = collections.Counter(spending_categories)
 
+top5 = spending_counter.most_common(5)
+
+print(top5)
